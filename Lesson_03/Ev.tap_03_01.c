838c63a75ab168c78636a97fed6c2b820ed4062e
LeylaM. <leyla.memmedova.eldar@gmail.com>
2026-03-03 05:39:33 +0400

    Create Ev.tap_03_01.c

---
+++ Lesson_03/Ev.tap_03_01.c
@@ -0,0 +1,5 @@
+Ders_03, Tapşırıq_04: getchar() və putchar()
+İstifadəçidən sətir oxuyub, hər sözün ilk hərfini böyük edən proqram yazın (getchar/putchar istifadə edin).
+Nümunə:
+Giriş: salam dünya
+Çıxış: Salam Dünya

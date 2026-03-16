e37492027f8f6837bb283c00d356fdc19d89c7d0
LeylaM. <leyla.memmedova.eldar@gmail.com>
2026-03-17 03:42:35 +0400

    Create Pre_Post.c

---
+++ Lesson_04/Pre_Post.c
@@ -0,0 +1,6 @@
+//++   Artırma: i++ (post), ++i (pre)
+//--   Azaltma: i-- (post), --i (pre)
+//Pre vs Post:
+int a = 5, b;
+b = a++;  // b=5, a=6 (post: əvvəl təyin et, sonra artır)
+b = ++a;  // b=7, a=7 (pre: əvvəl artır, sonra təyin et)

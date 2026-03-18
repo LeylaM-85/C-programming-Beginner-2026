8dba1d983b83ef5b67e9701043101e24b7592a6d
LeylaM. <leyla.memmedova.eldar@gmail.com>
2026-03-18 06:30:24 +0400

    Update Ayin_Gunleri_SWITC_CASE.C

--- Lesson_05/Ayin_Gunleri_SWITC_CASE.C
+++ Lesson_05/Ayin_Gunleri_SWITC_CASE.C
@@ -24,3 +24,44 @@ int main() {
 
     return 0;
 }
+
+
+
+/*2) Təqvim: Ayın günləri (switch-case qruplaşdırma ilə) enum siyahisindan istifade
+Bu nümunədə bir neçə case-i bir qrupda birləşdirərək kodun həcmini azaldırıq (Fevral ayı üçün 28 gün nəzərdə tutulub).*/
+#include <stdio.h>
+
+// İlk aya 1 mənimsətməklə siyahını başladırıq
+enum Aylar {
+    Yanvar = 1, Fevral, Mart, Aprel, May, Iyun, 
+    Iyul, Avqust, Sentyabr, Oktyabr, Noyabr, Dekabr
+};
+
+int main() {
+    int ay_no, gun = 0;
+    printf("Ayin nomresini daxil edin (1-12): ");
+    scanf("%d", &ay_no);
+
+    switch(ay_no) {
+        case Yanvar: case Mart: case May: case Iyul: 
+        case Avqust: case Oktyabr: case Dekabr:
+            gun = 31;
+            break;
+            
+        case Aprel: case Iyun: case Sentyabr: case Noyabr:
+            gun = 30;
+            break;
+            
+        case Fevral:
+            gun = 28; // Sadəlik üçün 28 götürdük
+            break;
+            
+        default:
+            printf("Yanlis ay nomresi!\n");
+            return 0;
+    }
+
+    printf("Bu ay %d gunden ibaretdir.", gun);
+
+    return 0;
+}

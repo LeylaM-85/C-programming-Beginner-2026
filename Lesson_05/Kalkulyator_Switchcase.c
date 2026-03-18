797c11ca8c57ed4816389a698be84b75f608e2b6
LeylaM. <leyla.memmedova.eldar@gmail.com>
2026-03-18 06:21:31 +0400

    Update Kalkulyator_Switchcase.c

--- Lesson_05/Kalkulyator_Switchcase.c
+++ Lesson_05/Kalkulyator_Switchcase.c
@@ -1,4 +1,4 @@
-//Hesablama maşını (switch-case ilə)
+//1. Hesablama maşını (switch-case ilə)
 #include <stdio.h>
 
 int main() {
@@ -22,3 +22,64 @@ int main() {
     }
     return 0;
 }
+
+
+//2. Hesablama maşını (switch-case ilə) getchar()
+#include <stdio.h>
+
+int main() {
+    char op;
+    float n1, n2;
+    
+    printf("Iki eded daxil edin: ");
+    scanf("%f %f", &n1, &n2);
+    
+    getchar(); //scanf-deki /n artiq ENTER-i silir
+    
+    printf("Operatoru daxil edin (+, -, *, /): ");
+    scanf("%c", &op);
+    
+
+    switch(op) {
+        case '+': printf("%.1f + %.1f = %.1f", n1, n2, n1 + n2); break;
+        case '-': printf("%.1f - %.1f = %.lf", n1, n2, n1 - n2); break;
+        case '*': printf("%.1f * %.1f = %.lf", n1, n2, n1 * n2); break;
+        case '/':
+            if(n2 != 0) printf("%.lf / %.lf = %.lf", n1, n2, n1 / n2);
+            else printf("Sifra bolmek olmaz!");
+            break;
+        default: printf("Sehv operator!");
+    }
+    return 0;
+}
+
+
+//3. Hesablama maşını (switch-case ilə) 
+#include <stdio.h>
+
+int main() {
+    char op;
+    float n1, n2;
+    
+    printf("Iki eded daxil edin: ");
+    scanf("%f %f", &n1, &n2);
+    
+     //scanf-deki /n artiq ENTER-i silmek ucun %C-d' c-nin qarsisina %-den evvel bir bosluq qoyuruq
+    
+    printf("Operatoru daxil edin (+, -, *, /): ");
+    scanf(" %c", &op);
+    
+
+    switch(op) {
+        case '+': printf("%.1f + %.1f = %.1f", n1, n2, n1 + n2); break;
+        case '-': printf("%.1f - %.1f = %.lf", n1, n2, n1 - n2); break;
+        case '*': printf("%.1f * %.1f = %.lf", n1, n2, n1 * n2); break;
+        case '/':
+            if(n2 != 0) printf("%.lf / %.lf = %.lf", n1, n2, n1 / n2);
+            else printf("Sifra bolmek olmaz!");
+            break;
+        default: printf("Sehv operator!");
+    }
+    return 0;
+}
+
